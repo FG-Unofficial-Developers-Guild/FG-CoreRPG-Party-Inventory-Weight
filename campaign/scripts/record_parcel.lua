b297b92ae45c4af22709f0f601034e67efa14cd4
Wil Thieme <wil.thieme@protonmail.com>
2022-03-24 21:05:51 -0400

    better line length and formatting

--- campaign/scripts/record_parcel.lua
+++ campaign/scripts/record_parcel.lua
@@ -1,49 +1,40 @@
 --
 --	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
-
 local function updateWeight()
-  local node_parcel = getDatabaseNode()
-  local node_party_coins = node_parcel.getChild('coinlist')
-  local node_party_items = node_parcel.getChild('itemlist')
+	local node_parcel = getDatabaseNode()
+	local node_party_coins = node_parcel.getChild('coinlist')
+	local node_party_items = node_parcel.getChild('itemlist')
 
-  local number_weight = ParcelWeight.calculateItemWeight(node_party_items) + ParcelWeight.calculateCoinWeight(node_party_coins)
-  DB.setValue(node_parcel, 'parcelweight', 'number', ParcelWeight.round(number_weight))
+	local number_weight = ParcelWeight.calculateItemWeight(node_party_items) + ParcelWeight.calculateCoinWeight(node_party_coins)
+	DB.setValue(node_parcel, 'parcelweight', 'number', ParcelWeight.round(number_weight))
 end
 
 local function manageHandlers(bRemove)
-  if bRemove then
-    DB.removeHandler("treasureparcels.*.coinlist.*", "onChildUpdate", updateWeight)
-    DB.removeHandler("treasureparcels.*.coinlist", "onChildDeleted", updateWeight)
+	if bRemove then
+		DB.removeHandler('treasureparcels.*.coinlist.*', 'onChildUpdate', updateWeight)
+		DB.removeHandler('treasureparcels.*.coinlist', 'onChildDeleted', updateWeight)
 
-    DB.removeHandler("treasureparcels.*.itemlist.*.count", "onUpdate", updateWeight)
-    DB.removeHandler("treasureparcels.*.itemlist.*.weight", "onUpdate", updateWeight)
-    DB.removeHandler("treasureparcels.*.itemlist", "onChildDeleted", updateWeight)
-  else
-    DB.addHandler("treasureparcels.*.coinlist.*", "onChildUpdate", updateWeight)
-    DB.addHandler("treasureparcels.*.coinlist", "onChildDeleted", updateWeight)
+		DB.removeHandler('treasureparcels.*.itemlist.*.count', 'onUpdate', updateWeight)
+		DB.removeHandler('treasureparcels.*.itemlist.*.weight', 'onUpdate', updateWeight)
+		DB.removeHandler('treasureparcels.*.itemlist', 'onChildDeleted', updateWeight)
+	else
+		DB.addHandler('treasureparcels.*.coinlist.*', 'onChildUpdate', updateWeight)
+		DB.addHandler('treasureparcels.*.coinlist', 'onChildDeleted', updateWeight)
 
-    DB.addHandler("treasureparcels.*.itemlist.*.count", "onUpdate", updateWeight)
-    DB.addHandler("treasureparcels.*.itemlist.*.weight", "onUpdate", updateWeight)
-    DB.addHandler("treasureparcels.*.itemlist", "onChildDeleted", updateWeight)
-  end
+		DB.addHandler('treasureparcels.*.itemlist.*.count', 'onUpdate', updateWeight)
+		DB.addHandler('treasureparcels.*.itemlist.*.weight', 'onUpdate', updateWeight)
+		DB.addHandler('treasureparcels.*.itemlist', 'onChildDeleted', updateWeight)
+	end
 end
 
 function onInit()
-  if super and super.onInit then
-    super.onInit()
-  end
-  if Session.IsHost then
-    manageHandlers()
-  end
-  updateWeight()
+	if super and super.onInit then super.onInit() end
+	if Session.IsHost then manageHandlers() end
+	updateWeight()
 end
 
 function onClose()
-  if super and super.onClose then
-    super.onClose()
-  end
-  if Session.IsHost then
-    manageHandlers(true)
-  end
+	if super and super.onClose then super.onClose() end
+	if Session.IsHost then manageHandlers(true) end
 end
--- ps/scripts/ps_weight_encumbrance.lua
+++ ps/scripts/ps_weight_encumbrance.lua
@@ -1,29 +1,24 @@
 function onInit()
-  if super and super.onInit then
-    super.onInit()
-  end
-  onValueChanged()
+	if super and super.onInit then super.onInit() end
+	onValueChanged()
 
 end
 
-
 function onValueChanged()
-  if super and super.onValueChanged then
-    super.onValueChanged()
-  end
+	if super and super.onValueChanged then super.onValueChanged() end
 
-  local nMax = window.maxweight.getValue() or 0
-  if nMax > 0 then
-    local nTotal = window.totalweight.getValue() or 0
-    local nPercentDmg = nTotal / nMax * 100
-    if nPercentDmg >= 100 then
-      window.totalweight.setColor(ColorManager.COLOR_HEALTH_CRIT_WOUNDS) --red
-    elseif nPercentDmg >= 50 then
-      window.totalweight.setColor(ColorManager.COLOR_HEALTH_HVY_WOUNDS) --orange
-    else
-      window.totalweight.setColor(ColorManager.COLOR_FULL) --black
-    end
-  else
-    window.totalweight.setColor(ColorManager.COLOR_FULL) --black
-  end
+	local nMax = window.maxweight.getValue() or 0
+	if nMax > 0 then
+		local nTotal = window.totalweight.getValue() or 0
+		local nPercentDmg = nTotal / nMax * 100
+		if nPercentDmg >= 100 then
+			window.totalweight.setColor(ColorManager.COLOR_HEALTH_CRIT_WOUNDS) -- red
+		elseif nPercentDmg >= 50 then
+			window.totalweight.setColor(ColorManager.COLOR_HEALTH_HVY_WOUNDS) -- orange
+		else
+			window.totalweight.setColor(ColorManager.COLOR_FULL) -- black
+		end
+	else
+		window.totalweight.setColor(ColorManager.COLOR_FULL) -- black
+	end
 end
