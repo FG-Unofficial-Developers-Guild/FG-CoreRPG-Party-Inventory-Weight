5187ac2ca3f62ea77b1fe287533722ea220ac931
Wil Thieme <wil.thieme@protonmail.com>
2024-02-23 19:14:07 -0500

    correct DB node names for parcel weight

--- campaign/scripts/record_parcel.lua
+++ campaign/scripts/record_parcel.lua
@@ -1,32 +1,15 @@
 --
 --	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
+
 local function updateWeight()
 	local node_parcel = getDatabaseNode()
 	local node_party_coins = node_parcel.getChild("coinlist")
 	local node_party_items = node_parcel.getChild("itemlist")
 
-	local number_weight = ParcelWeight.calculateItemWeight(node_party_items)
-		+ ParcelWeight.calculateCoinWeight(node_party_coins)
-	DB.setValue(node_parcel, "parcelweight", "number", ParcelWeight.round(number_weight))
-end
-
-local function manageHandlers(bRemove)
-	if bRemove then
-		DB.removeHandler("treasureparcels.*.coinlist.*", "onChildUpdate", updateWeight)
-		DB.removeHandler("treasureparcels.*.coinlist", "onChildDeleted", updateWeight)
-
-		DB.removeHandler("treasureparcels.*.itemlist.*.count", "onUpdate", updateWeight)
-		DB.removeHandler("treasureparcels.*.itemlist.*.weight", "onUpdate", updateWeight)
-		DB.removeHandler("treasureparcels.*.itemlist", "onChildDeleted", updateWeight)
-	else
-		DB.addHandler("treasureparcels.*.coinlist.*", "onChildUpdate", updateWeight)
-		DB.addHandler("treasureparcels.*.coinlist", "onChildDeleted", updateWeight)
-
-		DB.addHandler("treasureparcels.*.itemlist.*.count", "onUpdate", updateWeight)
-		DB.addHandler("treasureparcels.*.itemlist.*.weight", "onUpdate", updateWeight)
-		DB.addHandler("treasureparcels.*.itemlist", "onChildDeleted", updateWeight)
-	end
+	local item_weight = ParcelWeight.calculateItemWeight(node_party_items)
+	local coins_weight = ParcelWeight.calculateCoinWeight(node_party_coins)
+	DB.setValue(node_parcel, "parcelweight", "number", ParcelWeight.round(item_weight + coins_weight))
 end
 
 function onInit()
@@ -34,7 +17,12 @@ function onInit()
 		super.onInit()
 	end
 	if Session.IsHost then
-		manageHandlers()
+		DB.addHandler("treasureparcels.*.coinlist.*", "onChildUpdate", updateWeight)
+		DB.addHandler("treasureparcels.*.coinlist", "onChildDeleted", updateWeight)
+
+		DB.addHandler("treasureparcels.*.itemlist.*.count", "onUpdate", updateWeight)
+		DB.addHandler("treasureparcels.*.itemlist.*.weight", "onUpdate", updateWeight)
+		DB.addHandler("treasureparcels.*.itemlist", "onChildDeleted", updateWeight)
 	end
 	updateWeight()
 end
@@ -44,6 +32,11 @@ function onClose()
 		super.onClose()
 	end
 	if Session.IsHost then
-		manageHandlers(true)
+		DB.removeHandler("treasureparcels.*.coinlist.*", "onChildUpdate", updateWeight)
+		DB.removeHandler("treasureparcels.*.coinlist", "onChildDeleted", updateWeight)
+
+		DB.removeHandler("treasureparcels.*.itemlist.*.count", "onUpdate", updateWeight)
+		DB.removeHandler("treasureparcels.*.itemlist.*.weight", "onUpdate", updateWeight)
+		DB.removeHandler("treasureparcels.*.itemlist", "onChildDeleted", updateWeight)
 	end
 end
